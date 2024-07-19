b69efae36445e865287e591c8bce470639950cc0
devjylee <wbs0810@gmail.com>
2024-07-19 14:25:09 +0900

    setTimeout

---
+++ intermediate/setTimeout.js
@@ -0,0 +1,9 @@
+// function fn() {
+//   console.log(3);
+// }
+
+// setTimeout(fn, 3000);
+
+setTimeout(function () {
+  console.log(3);
+}, 3000);
---
+++ intermediate/setTimeout2.js
@@ -0,0 +1,6 @@
+function showName(name) {
+  console.log(name);
+}
+
+setTimeout(showName, 3000, "Mike");
+// setTimeout(함수, 시간, 인수);
